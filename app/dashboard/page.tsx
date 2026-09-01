a7e5c6fae51737a1ff7d5bf035cfbdcdf24e1ed0
Abdullrahman almaghrabi <73081830+Abdomghrbi@users.noreply.github.com>
2026-09-02 00:33:04 +0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -55,7 +55,7 @@ export default async function DashboardPage() {
             href="/dashboard/new"
             className="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-medium rounded-lg transition"
           >
-            + مقالة جديدة
+            +
           </Link>
         </div>
 
